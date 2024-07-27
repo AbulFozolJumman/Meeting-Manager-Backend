71806147f08c5648b66a80e8c9122949cdd31ff9
AbulFozolJumman <abulfozoljumman@gmail.com>
2024-07-28 00:04:35 +0600

    User interface and model creation done

---
+++ src/app/modules/User/user.model.ts
@@ -0,0 +1,35 @@
+import { model, Schema } from 'mongoose';
+import { TUser } from './user.interface';
+
+const UserSchema = new Schema<TUser>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['admin', 'user'],
+  },
+});
+
+const User = model<TUser>('user', UserSchema);
+
+export default User;
