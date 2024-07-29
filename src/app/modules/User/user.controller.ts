7ca119c152ed12accd3d15858d8ea7ad2755d075
AbulFozolJumman <abulfozoljumman@gmail.com>
2024-07-29 23:26:35 +0600

    User signup coding completed

---
+++ src/app/modules/User/user.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import { UserServices } from './user.service';
+import UserValidationSchema from './user.validation';
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const userData = req.body;
+    const validatedData = UserValidationSchema.parse(userData);
+    const result = await UserServices.createUserIntoDB(validatedData);
+    res.status(200).json({
+      success: true,
+      statusCode: 200,
+      message: 'User registered successfully',
+      data: result,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      statusCode: 500,
+      message: err.message,
+      data: [],
+    });
+  }
+};
+
+export const UserController = {
+  createUser,
+};
---
+++ src/app/modules/User/user.validation.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+const UserValidationSchema = z.object({
+  name: z.string().min(3),
+  email: z.string().email().min(9),
+  password: z.string().min(8),
+  phone: z.number().min(1),
+  address: z.string().min(1),
+  role: z.enum(['user', 'admin']).default('user'),
+});
+
+export default UserValidationSchema;
