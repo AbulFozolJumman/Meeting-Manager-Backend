8dfa69060b69ba35abed90775c6e3c741301727f
AbulFozolJumman <abulfozoljumman@gmail.com>
2024-08-16 23:10:53 +0600

    Rooms all error solving coding completed

--- src/app/modules/Room/room.controller.ts
+++ src/app/modules/Room/room.controller.ts
@@ -1,21 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response } from 'express';
 import { RoomServices } from './room.service';
-import { RoomValidation } from './room.validation';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
 
-const createRoom = catchAsync(async (req: Request, res: Response) => {
-  const user = req.user;
-  if (user.role !== 'admin') {
-    return res.status(httpStatus.FORBIDDEN).json({
-      success: false,
-      statusCode: httpStatus.FORBIDDEN,
-      message: 'Access denied. Only admins can create rooms.',
-    });
-  }
-
+const createRoom = catchAsync(async (req, res) => {
   const result = await RoomServices.createRoomIntoDB(req.body);
 
   sendResponse(res, {
@@ -26,87 +14,45 @@ const createRoom = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
-const getSingleRoom = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const result = await RoomServices.getSingleRoomFromDB(id);
-    res.status(200).json({
-      success: true,
-      statusCode: 200,
-      message: 'Room retrieved successfully',
-      data: result,
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      statusCode: 500,
-      message: err.message,
-      data: [],
-    });
-  }
-};
+const getSingleRoom = catchAsync(async (req, res) => {
+  const result = await RoomServices.getSingleRoomFromDB(req.params.id);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Room retrieved successfully',
+    data: result,
+  });
+});
 
-const getAllRoom = async (req: Request, res: Response) => {
-  try {
-    const result = await RoomServices.getAllRoomFromDB();
-    res.status(200).json({
-      success: true,
-      statusCode: 200,
-      message: 'Rooms retrieved successfully',
-      data: result,
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      statusCode: 500,
-      message: err.message,
-      data: [],
-    });
-  }
-};
+const getAllRoom = catchAsync(async (req, res) => {
+  const result = await RoomServices.getAllRoomFromDB();
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Rooms retrieved successfully',
+    data: result,
+  });
+});
 
-const updateRoom = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const roomData = req.body;
-    const validatedRoomData =
-      RoomValidation.updateRoomValidationSchema.parse(roomData);
-    const result = await RoomServices.updateRoomIntoDB(id, validatedRoomData);
-    res.status(200).json({
-      success: true,
-      statusCode: 200,
-      message: 'Room updated successfully',
-      data: result,
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      statusCode: 500,
-      message: err.message,
-      data: [],
-    });
-  }
-};
+const updateRoom = catchAsync(async (req, res) => {
+  const result = await RoomServices.updateRoomIntoDB(req.params.id, req.body);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Room updated successfully',
+    data: result,
+  });
+});
 
-const deleteRoom = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const result = await RoomServices.deleteRoomIntoDB(id);
-    res.status(200).json({
-      success: true,
-      statusCode: 200,
-      message: 'Room deleted successfully',
-      data: result,
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      success: false,
-      statusCode: 500,
-      message: err.message,
-      data: [],
-    });
-  }
-};
+const deleteRoom = catchAsync(async (req, res) => {
+  const result = await RoomServices.deleteRoomIntoDB(req.params.id);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Room deleted successfully',
+    data: result,
+  });
+});
 
 export const RoomController = {
   createRoom,
--- src/app/modules/Room/room.route.ts
+++ src/app/modules/Room/room.route.ts
@@ -2,17 +2,29 @@ import express from 'express';
 import { RoomController } from './room.controller';
 import auth from '../../middlewares/auth';
 import { Roles } from '../User/user.enumeration';
+import { RoomValidation } from './room.validation';
+import validateRequest from '../../middlewares/validateRequest';
 
 const router = express.Router();
 
-router.post('/', auth(Roles.admin), RoomController.createRoom);
+router.post(
+  '/',
+  validateRequest(RoomValidation.createRoomValidationSchema),
+  auth(Roles.admin),
+  RoomController.createRoom,
+);
 
 router.get('/:id', RoomController.getSingleRoom);
 
 router.get('/', RoomController.getAllRoom);
 
-router.put('/:id', RoomController.updateRoom);
+router.put(
+  '/:id',
+  auth(Roles.admin),
+  validateRequest(RoomValidation.updateRoomValidationSchema),
+  RoomController.updateRoom,
+);
 
-router.delete('/:id', RoomController.deleteRoom);
+router.delete('/:id', auth(Roles.admin), RoomController.deleteRoom);
 
 export const RoomRoutes = router;
